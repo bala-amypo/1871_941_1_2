62ce1aa78bd4240df78b572c6fc2ed0647e6e08d
bala-amypo <bala@amypo.in>
2025-12-15 14:48:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -0,0 +1,9 @@
+package com.example.demo.servlet;
+
+import com.example.demo.servlet.SimpleStatusServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import 
+
+public class SimpleStatusServlet {
+    
+}
